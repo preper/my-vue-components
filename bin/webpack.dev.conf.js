76a0f76e831bdca037d1fa1648bf848b8d36ffde
preper <qpreper@hotmail.com>
2020-08-07 20:02:38 +0800

    完成dev构建

---
+++ bin/webpack.dev.conf.js
@@ -0,0 +1,174 @@
+'use strict'
+const path = require('path')
+const portfinder = require('portfinder')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const packageConfig = require('../package.json')
+
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
+
+function createNotifierCallback () {
+  const notifier = require('node-notifier')
+
+  return (severity, errors) => {
+    if (severity !== 'error') return
+
+    const error = errors[0]
+    const filename = error.file && error.file.split('!').pop()
+
+    notifier.notify({
+      title: packageConfig.name,
+      message: severity + ': ' + error.name,
+      subtitle: filename || '',
+      icon: path.join(__dirname, 'logo.png')
+    })
+  }
+}
+
+const devWebpackConfig = {
+  context: path.resolve(__dirname, '../'),
+  entry: {
+    app: './examples/index.js'
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.join(__dirname, '../src')
+    },
+    extensions: ['.js', '.vue', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          transformAssetUrls: {
+            video: ['src', 'poster'],
+            source: 'src',
+            img: 'src',
+            image: 'xlink:href'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.join(__dirname, '../src'), path.join(__dirname, '../examples')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/img/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/fonts/[name].[hash:7].[ext]'
+        }
+      }
+    ]
+  },
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+
+  devServer: {
+    clientLogLevel: 'warning',
+    historyApiFallback: {
+      rewrites: [
+        { from: /.*/, to: path.posix.join('examples', 'index.html') },
+      ],
+    },
+    hot: true,
+    contentBase: false,
+    compress: true,
+    host: HOST || 'localhost',
+    port: PORT || 8080,
+    open: false,
+    overlay: { warnings: false, errors: true },
+    publicPath: '/',
+    proxy: {},
+    quiet: true,
+    watchOptions: {
+      poll: false,
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'examples/index.html',
+      inject: true
+    })
+  ],
+  node: {
+    setImmediate: false,
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+}
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = process.env.PORT || 8080
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      process.env.PORT = port
+      devWebpackConfig.devServer.port = port
+
+      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+        },
+        onErrors: createNotifierCallback()
+      }))
+
+      resolve(devWebpackConfig)
+    }
+  })
+})
